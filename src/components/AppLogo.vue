283b7c4f5fedf9d5b6f4c3ce3f0b6be464ac79d0
MartinsOnuoha <martinsvictor.nd@gmail.com>
2022-04-05 00:43:32 +0100

    feat(components): setup app layout

---
+++ src/components/AppLogo.vue
@@ -0,0 +1,11 @@
+<template>
+  <img width="150" src="../assets/logo.svg" alt="" srcset="">
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'AppLogo',
+});
+</script>
--- src/entities/Task.ts
+++ src/entities/Task.ts
@@ -1,10 +1,12 @@
 export interface SubTask {
   id: string;
   title: string;
+  taskId: string;
   createdAt: Date;
 }
 
 export interface Task {
+  id: string;
   title: string
   subtasks: SubTask[]
 }
