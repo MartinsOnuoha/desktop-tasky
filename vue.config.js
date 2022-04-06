f12636aa9950f68143f8fced87b3682bd497d93e
MartinsOnuoha <martinsvictor.nd@gmail.com>
2022-04-06 08:28:09 +0100

    🐛 fix(desktop-tasky.yml): add GH_TOKEN

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,16 @@ module.exports = {
   pluginOptions: {
     electronBuilder: {
       nodeIntegration: true,
+      builderOptions: {
+        appId: 'com.desktop.tasky',
+        productName: 'Desktop Tasky',
+        copyright: 'Copyright © 2022 MartinsOnuoha',
+        mac: {
+          category: 'public.app-category.productivity',
+          target: 'dmg',
+          icon: 'src/assets/logo.png',
+        },
+      },
     },
     i18n: {
       locale: 'en',
